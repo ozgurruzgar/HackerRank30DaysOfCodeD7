c004e6ed3eca73613c2d6b48e6fdc724309169a0
Özgür <zgr@DESKTOP-1NLBACI>
2022-12-03 04:30:40 +0300

    Add project files.

---
+++ HackerRank30DaysOfCodeD7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank30DaysOfCodeD7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            ReversePrint(arr);
+        }
+        private static void ReversePrint(List<int> arr)
+        {
+            int Length = arr.Count;
+            for (int i = Length - 1; i >= 0; i--)
+            {
+                if (i != 0)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+                else
+                {
+                    Console.Write(arr[i]);
+                }
+            }
+        }
+    }
+}
